675c1857b8bd20a0a7e613ffb22959360a5de983
Jaimaica <97622981+aciamiaj@users.noreply.github.com>
2022-07-16 14:02:23 -0400

    complete  module-9/assignment

---
+++ module-9/assignments/customers-async-xhr.js
@@ -0,0 +1,88 @@
+function displayData(xml) {
+    const displayElement = document.getElementById(`customers`);
+    const xmlDoc = xml;             
+    const customerNodes = xmlDoc.getElementsByTagName("customer");
+
+   for (let index = 0; index < customerNodes.length; index++) {
+        const customer = customerNodes[index];
+        const displayItem = document.createElement(`li`);
+
+        const id = document.createElement(`h3`);
+        id.appendChild(
+            document.createTextNode(
+                customer.getAttribute(`custID`))
+        );
+        displayItem.appendChild(id);
+
+        const subtitle = document.createElement(`p`);
+        subtitle.appendChild(
+            document.createTextNode(
+                customer.getElementsByTagName("name")[0].childNodes[0]
+                    .nodeValue)
+        );
+        displayItem.appendChild(subtitle);
+
+        const custAdd = document.createElement(`p`);
+        custAdd.appendChild(
+            document.createTextNode(
+                customer.getElementsByTagName("address")[0].childNodes[0]
+                    .nodeValue)
+        );
+        displayItem.appendChild(custAdd);
+
+        const cphone = document.createElement(`p`);
+        cphone.appendChild(
+            document.createTextNode(
+                customer.getElementsByTagName("phone")[0].childNodes[0]
+                    .nodeValue)
+        );
+        displayItem.appendChild(cphone);
+
+        const cemail = document.createElement(`p`);
+        cemail.appendChild(
+            document.createTextNode(
+                customer.getElementsByTagName("email")[0].childNodes[0]
+                    .nodeValue)
+        );
+        displayItem.appendChild(cemail);
+
+        const cOrderDate = document.createElement(`p`);
+        cOrderDate.appendChild(
+            document.createTextNode(
+                customer.getElementsByTagName("orderDate")[0].childNodes[0]
+                    .nodeValue)
+         );
+         displayItem.appendChild(cOrderDate);
+
+         const cItemPrice = document.createElement(`p`);
+         cItemPrice.appendChild(
+            document.createTextNode(
+                customer.getElementsByTagName("itemPrice")[0].childNodes[0]
+                    .nodeValue)
+         );
+         displayItem.appendChild(cItemPrice);
+
+         const cItemQty = document.createElement(`p`);
+         cItemQty.appendChild(
+            document.createTextNode(
+                customer.getElementsByTagName("itemQty")[0].childNodes[0]
+                    .nodeValue)
+         );
+         displayItem.appendChild(cItemQty);
+
+        displayElement.appendChild(displayItem);
+    } 
+}
+
+const loadData = async () => {
+    try {
+        const response = await fetch(`customers.xml`);
+        const str = await response.text();
+        const data = new DOMParser().parseFromString(str, `text/xml`);
+        displayData(data);
+    } catch (error){
+        console.error(error);
+    }
+};
+
+loadData();
